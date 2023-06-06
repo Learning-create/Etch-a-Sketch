1a5f12b978c54ef6fd97d2dc37bf0ec9c7d31f49
Sam <dacosta.peon@gmail.com>
2023-06-06 02:46:20 +0200

    grid working, added btn to change size

--- myScript.js
+++ myScript.js
@@ -1,7 +1,12 @@
 const container = document.getElementById("container");
-let num = 16;
+let num = 5;
+
+function changeGrid() {
+
+    test = prompt("please enter a number", "10");
 
 
+};
 
 for(i = 0; i < num; i++) {
 
@@ -14,12 +19,13 @@ for(i = 0; i < num; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         row.appendChild(cell);
-
+ 
+cell.addEventListener("mouseenter", function( event ) {   
+    event.target.style.backgroundColor = "blue";
+  }, false);
+/*this clears the cells as you leave the cell
+  cell.addEventListener("mouseleave", function( event ) {   
+    event.target.style.backgroundColor = "white";
+  }, false); */
     }
 }
-
-function mouseOver( {
-
-
-
-})
