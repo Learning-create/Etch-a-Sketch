5bf749ba6ec08c251344985a41d0642d05281a84
Sam <dacosta.peon@gmail.com>
2023-06-06 20:37:50 +0200

    Button to change number of cells works

--- myScript.js
+++ myScript.js
@@ -1,31 +1,67 @@
 const container = document.getElementById("container");
-let num = 5;
+let num = 2;
+
+function createGrid() {
+
+  for(i = 0; i < num; i++) {
+
+      let newRow = document.createElement("div");
+      newRow.classList.add("row");
+      container.appendChild(newRow);
+
+      for(a = 0; a< num; a++){
+
+        let newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        newRow.appendChild(newCell);
+  
+        newCell.addEventListener("mouseenter", function( event ) {   
+          event.target.style.backgroundColor = "blue";
+        });
+
+        /*this would clear the cells as you leave the cell
+        cell.addEventListener("mouseleave", function( event ) {   
+          event.target.style.backgroundColor = "white";
+        }, false); */
+
+      }
+  }
+
+}
 
 function changeGrid() {
 
     test = prompt("please enter a number", "10");
 
+    test = Number(test);
+
+
+    if (Number.isNaN(test)) {
+      
+      alert("Must input a number!");
+      
+    } else if (test < 0) {
+
+      alert("Must be a positive number");
+
+    } else if (test > 100) {
+
+      alert("Must be a number bellow 100");
+
+    } else {
+
+      num = test;
+
+      let row = document.getElementsByClassName("row");
+
+      while(row.length > 0) {
+        row[0].parentNode.removeChild(row[0]);
+      };
+
+      createGrid();
+
+    }
+
 
 };
-
-for(i = 0; i < num; i++) {
-
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-
-    for(a = 0; a< num; a++){
-
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
- 
-cell.addEventListener("mouseenter", function( event ) {   
-    event.target.style.backgroundColor = "blue";
-  }, false);
-/*this clears the cells as you leave the cell
-  cell.addEventListener("mouseleave", function( event ) {   
-    event.target.style.backgroundColor = "white";
-  }, false); */
-    }
-}
+createGrid();
